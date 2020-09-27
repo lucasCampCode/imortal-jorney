79f7d206d9b28fe0a12e5cc761abba82447f1301
lucas <lcampbell0815@gmail.com>
2020-09-27 17:25:00 -0500

    added input control for player; added players name

--- Game.cs
+++ Game.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
     class Game
     {
         private bool _gameOver = false;
+        private Entity _player1;
 
         //Run the game
         public void Run()
@@ -19,12 +20,160 @@ namespace HelloWorld
             End();
         }
 
+        //possible ways to take ask a question for the user and return input
+        public void GetInput(out char input, string option1, string option2, string query)
+        {
+            Console.WriteLine(query);
+            Console.WriteLine("1." + option1);
+            Console.WriteLine("2." + option2);
+            Console.Write("> ");
 
+            input = ' ';
+            while (input != '1' && input != '2')
+            {
+                input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if (input != '1' && input != '2')
+                {
+                    Console.WriteLine("invalid input!");
+                }
+            }
+            Console.Clear();
+        }
+        public void GetInput(out char input, string option1, string option2, string option3, string query)
+        {
+            Console.WriteLine(query);
+            Console.WriteLine("1." + option1);
+            Console.WriteLine("2." + option2);
+            Console.WriteLine("3." + option3);
+            Console.Write("> ");
+
+            input = ' ';
+            while (input != '1' && input != '2' && input != '3')
+            {
+                input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if (input != '1' && input != '2' && input != '3')
+                {
+                    Console.WriteLine("invalid input!");
+                }
+            }
+            Console.Clear();
+        }
+        public void GetInput(out char input, string option1, string option2, string option3, string query, bool messUp)
+        {
+            Console.WriteLine(query);
+            Console.WriteLine("1." + option1);
+            Console.WriteLine("2." + option2);
+            Console.WriteLine("3." + option3);
+            Console.Write("> ");
+
+            input = ' ';
+            if (messUp == false)
+            {
+                while (input != '1' && input != '2' && input != '3')
+                {
+                    input = Console.ReadKey().KeyChar;
+                    Console.WriteLine();
+                    if (input != '1' && input != '2' && input != '3')
+                    {
+                        Console.WriteLine("invalid input!");
+                    }
+                }
+            }
+            else
+            {
+                input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            }
+            Console.Clear();
+
+        }
+        public void GetInput(out char input, string option1, string option2, string option3, string option4, string query)
+        {
+            Console.WriteLine(query);
+            Console.WriteLine("1." + option1);
+            Console.WriteLine("2." + option2);
+            Console.WriteLine("3." + option3);
+            Console.WriteLine("4." + option4);
+            Console.Write("> ");
+
+            input = ' ';
+            while (input != '1' && input != '2' && input != '3' && input != '4')
+            {
+                input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if (input != '1' && input != '2' && input != '3' && input != '4')
+                {
+                    Console.WriteLine("invalid input!");
+                }
+            }
+            Console.Clear();
+        }
+        public void GetInput(out char input, string option1, string option2, string option3, string option4, string query, bool messUp)
+        {
+            Console.WriteLine(query);
+            Console.WriteLine("1." + option1);
+            Console.WriteLine("2." + option2);
+            Console.WriteLine("3." + option3);
+            Console.WriteLine("4." + option4);
+            Console.Write("> ");
+
+            input = ' ';
+            if (messUp == false)
+            {
+                while (input != '1' && input != '2' && input != '3' && input != '4')
+                {
+                    input = Console.ReadKey().KeyChar;
+                    Console.WriteLine();
+                    if (input != '1' && input != '2' && input != '3' && input != '4')
+                    {
+                        Console.WriteLine("invalid input!");
+                    }
+                }
+            }
+            else
+            {
+                input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            }
+            Console.Clear();
+        }
+
+
+        //give user instructions 
+        public void Intro()
+        {
+            string name;
+            char input = ' ';
+            Console.WriteLine("Good morning and welcome");
+            Console.WriteLine("let's start with introductions!");
+            Console.WriteLine("What's your name traveler?");
+            name = Console.ReadLine();
+            while(input != '1')
+            {
+                GetInput(out input, "yes", "change it", "do you want to keep this name");
+                switch (input)
+                {
+                    case '1':
+                        {
+                            Console.WriteLine("your forever name will be " + name + "!");
+                            _player1 = new Entity(name, 100, 10);
+                            break;
+                        }
+                    case '2':
+                        {
+                            Console.WriteLine("Alright lets change that name of yours");
+                            break;
+                        }
+                }
+            }
+        }
 
         //Performed once when the game begins
         public void Start()
         {
-            
+            Intro();
         }
 
         //Repeated until the game ends
